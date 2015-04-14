e8086edb64a117229b925e602a885b4ecec61166
Dale Campbell <oshuma@gmail.com>
2015-04-14 03:31:24 -0400

    Parse URLs into map, keyed on the message's nick.

--- scumbot.go
+++ scumbot.go
@@ -3,8 +3,14 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"regexp"
 
 	irc "github.com/fluffle/goirc/client"
+)
+
+var (
+	// TODO: Map to store URLs keyed by nick. Store these in a database.
+	urlDatabase = make(map[string][]string)
 )
 
 func main() {
@@ -35,18 +41,51 @@ func main() {
 		quit <- true
 	})
 
-	client.HandleFunc("PRIVMSG", func(conn *irc.Conn, line *irc.Line) {
-		nick := line.Nick
-		msg := line.Args[1]
-		time := line.Time
-		fmt.Printf("-> MSG(%s): %s: %s\n", time, nick, msg)
-	})
+	client.HandleFunc("PRIVMSG", msgHandler)
 
 	// Tell client to connect.
 	if err := client.Connect(); err != nil {
 		fmt.Printf("Connection error: %s\n", err)
+		quit <- true
 	}
 
 	// Wait for disconnect.
 	<-quit
+}
+
+func msgHandler(conn *irc.Conn, line *irc.Line) {
+	time := line.Time
+	nick := line.Nick
+	msg := line.Args[1]
+
+	fmt.Printf("<- MSG(%s) %s: %s\n", time, nick, msg)
+
+	saveURLs(nick, msg)
+
+	fmt.Printf("-> URLs: %s\n", urlDatabase)
+}
+
+func saveURLs(nick string, msg string) {
+	re := regexp.MustCompile(`((ftp|git|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(?:\/|\/([\w#!:.?+=&%@!\-\/]))?)`)
+
+	if urlMatches := re.FindAllString(msg, -1); urlMatches != nil {
+		for _, url := range urlMatches {
+			if notInArray(url, urlDatabase[nick]) {
+				urlDatabase[nick] = append(urlDatabase[nick], url)
+			}
+		}
+	}
+}
+
+func notInArray(value string, array []string) bool {
+	return !inArray(value, array)
+}
+
+func inArray(value string, array []string) bool {
+	for _, v := range array {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
