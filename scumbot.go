1ad3c3813a04f06627f9feca37364f832f0a50cd
Dale Campbell <oshuma@gmail.com>
2015-04-14 23:21:30 -0400

    Refactoring some shit.

--- scumbot.go
+++ scumbot.go
@@ -8,49 +8,72 @@ import (
 	irc "github.com/fluffle/goirc/client"
 )
 
+type Scumbot struct {
+	Client *irc.Conn
+	Config map[string]string
+}
+
 var (
-	// TODO: Map to store URLs keyed by nick. Store these in a database.
+	// TODO: Map to store URLs keyed by nick. Store this in a database.
 	urlDatabase = make(map[string][]string)
+
+	bot Scumbot
+
+	// Channel to handle disconnect.
+	quit = make(chan bool)
 )
+
+func NewBot() Scumbot {
+	fmt.Println("-> Setting up bot...")
+
+	botConfig := map[string]string{
+		"name":      "scumbot",
+		"database":  "scumbag",
+		"ircServer": "irc.literat.us:9999",
+	}
+
+	clientConfig := irc.NewConfig(botConfig["name"])
+	clientConfig.Server = botConfig["ircServer"]
+
+	// Setup SSL and skip cert verify.
+	clientConfig.SSL = true
+	clientConfig.SSLConfig = new(tls.Config)
+	clientConfig.SSLConfig.InsecureSkipVerify = true
+
+	clientConfig.NewNick = func(n string) string { return n + "^" }
+
+	return Scumbot{Client: irc.Client(clientConfig), Config: botConfig}
+}
 
 func main() {
 	fmt.Println("-> Starting...")
 
-	config := irc.NewConfig("scumbot")
-	config.Server = "irc.example.com:1234"
+	bot = NewBot()
+	setupHandlers()
 
-	// Setup SSL and skip cert verify.
-	config.SSL = true
-	config.SSLConfig = new(tls.Config)
-	config.SSLConfig.InsecureSkipVerify = true
-
-	config.NewNick = func(n string) string { return n + "^" }
-
-	client := irc.Client(config)
-
-	// Channel to handle disconnect.
-	quit := make(chan bool)
-
-	client.HandleFunc("CONNECTED", func(conn *irc.Conn, line *irc.Line) {
-		fmt.Println("-> Connecting to #scumbot")
-		conn.Join("#scumbot")
-	})
-
-	client.HandleFunc("DISCONNECTED", func(conn *irc.Conn, line *irc.Line) {
-		fmt.Println(" -> Disconnected...")
-		quit <- true
-	})
-
-	client.HandleFunc("PRIVMSG", msgHandler)
-
-	// Tell client to connect.
-	if err := client.Connect(); err != nil {
+	if err := bot.Client.Connect(); err != nil {
 		fmt.Printf("Connection error: %s\n", err)
 		quit <- true
 	}
 
 	// Wait for disconnect.
 	<-quit
+}
+
+func setupHandlers() {
+	fmt.Println("-> Setting up handlers...")
+
+	bot.Client.HandleFunc("CONNECTED", func(conn *irc.Conn, line *irc.Line) {
+		fmt.Println("-> Connecting to #scumbot")
+		conn.Join("#scumbot")
+	})
+
+	bot.Client.HandleFunc("DISCONNECTED", func(conn *irc.Conn, line *irc.Line) {
+		fmt.Println(" -> Disconnected...")
+		quit <- true
+	})
+
+	bot.Client.HandleFunc("PRIVMSG", msgHandler)
 }
 
 func msgHandler(conn *irc.Conn, line *irc.Line) {
@@ -60,16 +83,16 @@ func msgHandler(conn *irc.Conn, line *irc.Line) {
 
 	fmt.Printf("<- MSG(%s) %s: %s\n", time, nick, msg)
 
-	saveURLs(nick, msg)
+	bot.saveURLs(nick, msg)
 
 	fmt.Printf("-> URLs: %s\n", urlDatabase)
 }
 
-func saveURLs(nick string, msg string) {
+func (b Scumbot) saveURLs(nick string, msg string) {
 	re := regexp.MustCompile(`((ftp|git|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(?:\/|\/([\w#!:.?+=&%@!\-\/]))?)`)
 
-	if urlMatches := re.FindAllString(msg, -1); urlMatches != nil {
-		for _, url := range urlMatches {
+	if urls := re.FindAllString(msg, -1); urls != nil {
+		for _, url := range urls {
 			if notInArray(url, urlDatabase[nick]) {
 				urlDatabase[nick] = append(urlDatabase[nick], url)
 			}
