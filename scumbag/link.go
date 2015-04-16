e3c4b5c3cc4e3ac10039c504c2b60672b4be9b69
Dale Campbell <oshuma@gmail.com>
2015-04-16 03:11:37 -0400

    Moving SaveURLs to link.go

--- scumbag/link.go
+++ scumbag/link.go
@@ -1,11 +1,43 @@
 package scumbag
 
 import (
+	"fmt"
+	"regexp"
 	"time"
+
+	irc "github.com/fluffle/goirc/client"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Link struct {
 	Nick      string
 	Url       string
 	Timestamp time.Time
+}
+
+func SaveURLs(bot *Scumbag, line *irc.Line) {
+	nick := line.Nick
+	msg := line.Args[1]
+
+	re := regexp.MustCompile(`((ftp|git|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(?:\/|\/([\w#!:.?+=&%@!\-\/]))?)`)
+
+	if urls := re.FindAllString(msg, -1); urls != nil {
+		for _, url := range urls {
+			link := Link{}
+
+			if err := bot.Links.Find(bson.M{"nick": nick, "url": url}).One(&link); err != nil {
+				// Link doesn't exist, so create one.
+				link.Nick = nick
+				link.Url = url
+				link.Timestamp = line.Time
+
+				if err := bot.Links.Insert(link); err != nil {
+					fmt.Println("ERROR: ", err)
+					continue // With the next URL match.
+				} else {
+					fmt.Printf("-> LINK: %v\n", link)
+				}
+			}
+		}
+	}
 }
--- scumbag/scumbag.go
+++ scumbag/scumbag.go
@@ -3,11 +3,9 @@ package scumbag
 import (
 	"crypto/tls"
 	"fmt"
-	"regexp"
 
 	irc "github.com/fluffle/goirc/client"
 	mgo "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 var (
@@ -108,32 +106,5 @@ func (bot *Scumbag) setupHandlers() {
 func (bot *Scumbag) msgHandler(conn *irc.Conn, line *irc.Line) {
 	fmt.Printf("<- MSG(%v) %v: %v\n", line.Time, line.Nick, line.Args)
 
-	bot.saveURLs(line)
-}
-
-func (bot *Scumbag) saveURLs(line *irc.Line) {
-	nick := line.Nick
-	msg := line.Args[1]
-
-	re := regexp.MustCompile(`((ftp|git|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(?:\/|\/([\w#!:.?+=&%@!\-\/]))?)`)
-
-	if urls := re.FindAllString(msg, -1); urls != nil {
-		for _, url := range urls {
-			link := Link{}
-
-			if err := bot.Links.Find(bson.M{"nick": nick, "url": url}).One(&link); err != nil {
-				// Link doesn't exist, so create one.
-				link.Nick = nick
-				link.Url = url
-				link.Timestamp = line.Time
-
-				if err := bot.Links.Insert(link); err != nil {
-					fmt.Println("ERROR: ", err)
-					continue // With the next URL match.
-				} else {
-					fmt.Printf("-> LINK: %v\n", link)
-				}
-			}
-		}
-	}
+	SaveURLs(bot, line)
 }
