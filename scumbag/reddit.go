f058678ee80b58c3b76d227c5aeffd2a3ee17a21
Dale Campbell <oshuma@gmail.com>
2017-01-21 03:21:44 +0000

    Skip 'self.' posts when getting latest post.

--- scumbag/reddit.go
+++ scumbag/reddit.go
@@ -1,14 +1,20 @@
 package scumbag
 
 import (
+	"errors"
+	"regexp"
 	"strings"
 
 	"github.com/jzelinskie/geddit"
 )
 
+var (
+	selfPostRegexp = regexp.MustCompile(`\Aself\.`)
+)
+
 func (bot *Scumbag) HandleRedditCommand(channel string, subreddit string) {
 	opts := geddit.ListingOptions{
-		Limit: 1,
+		Limit: 5,
 	}
 
 	submissions, err := bot.Reddit.SubredditSubmissions(subreddit, geddit.HotSubmissions, opts)
@@ -17,9 +23,23 @@ func (bot *Scumbag) HandleRedditCommand(channel string, subreddit string) {
 		return
 	}
 
-	for _, submission := range submissions {
-		// This is needed because the URL returned has HTML escaped params for some dumbass reason.
-		url := strings.Replace(submission.URL, "&amp;", "&", -1)
-		bot.Msg(channel, url)
+	submission, err := getLatestSubmission(submissions)
+	if err != nil {
+		bot.Log.WithField("error", err).Error("HandleRedditCommand()")
+		return
 	}
+
+	// This is needed because the URL returned has HTML escaped params for some dumbass reason.
+	url := strings.Replace(submission.URL, "&amp;", "&", -1)
+	bot.Msg(channel, url)
+}
+
+func getLatestSubmission(submissions []*geddit.Submission) (*geddit.Submission, error) {
+	for _, submission := range submissions {
+		if selfPostRegexp.Find([]byte(submission.Domain)) == nil {
+			return submission, nil
+		}
+	}
+
+	return nil, errors.New("No real submission found")
 }
