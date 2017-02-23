f549f155becfa5428a193867029bd2ad03333222
Dale Campbell <oshuma@gmail.com>
2017-02-23 04:04:02 +0000

    Handle PR event.

--- scumbag/github.go
+++ scumbag/github.go
@@ -21,8 +21,9 @@ type GithubRepo struct {
 }
 
 type GithubPayload struct {
-	Commits []GithubEventCommit `json:"commits"`
-	Comment GithubEventComment  `json:"comment"`
+	Commits     []GithubEventCommit    `json:"commits"`
+	Comment     GithubEventComment     `json:"comment"`
+	PullRequest GithubEventPullRequest `json:"pull_request"`
 }
 
 type GithubEventCommit struct {
@@ -33,6 +34,12 @@ type GithubEventCommit struct {
 type GithubEventComment struct {
 	Body    string `json:"body"`
 	HtmlUrl string `json:"html_url"`
+}
+
+type GithubEventPullRequest struct {
+	HtmlUrl string `json:"html_url"`
+	Title   string `json:"title"`
+	Body    string `json:"body"`
 }
 
 type GithubCommit struct {
@@ -70,6 +77,8 @@ func (bot *Scumbag) HandleGithubCommand(channel string, username string) {
 			pushEvent(bot, channel, event)
 		case "IssueCommentEvent":
 			issueCommentEvent(bot, channel, event)
+		case "PullRequestEvent":
+			pullRequestEvent(bot, channel, event)
 		default:
 			bot.Log.WithField("event", event).Warn("HandleGithubCommand(): Unhandled event")
 		}
@@ -104,4 +113,10 @@ func issueCommentEvent(bot *Scumbag, channel string, event GithubEvent) {
 	eventMsg := fmt.Sprintf("%s: %s", event.Repo.Name, event.Payload.Comment.Body)
 	bot.Msg(channel, eventMsg)
 	bot.Msg(channel, event.Payload.Comment.HtmlUrl)
+}
+
+func pullRequestEvent(bot *Scumbag, channel string, event GithubEvent) {
+	eventMsg := fmt.Sprintf("%s: PR: %s", event.Repo.Name, event.Payload.PullRequest.Title)
+	bot.Msg(channel, eventMsg)
+	bot.Msg(channel, event.Payload.PullRequest.HtmlUrl)
 }
