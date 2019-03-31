5dbf9204eff7afd96011d4254055df1a16f68a3d
Dale Campbell <oshuma@gmail.com>
2019-03-30 22:37:59 -0400

    Adding '?game -recent' subcommand.

--- scumbag/game.go
+++ scumbag/game.go
@@ -3,7 +3,6 @@ package scumbag
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
@@ -14,14 +13,18 @@ import (
 )
 
 const (
-	igdbAPIURL   = "https://api-v3.igdb.com"
-	igdbGamesURL = igdbAPIURL + "/games"
+	igdbAPIURL          = "https://api-v3.igdb.com"
+	igdbGamesURL        = igdbAPIURL + "/games"
+	igdbReleaseDatesURL = igdbAPIURL + "/release_dates"
+
+	igdbLimit     = 5
+	igdbTimeframe = 7
 )
 
 var gameHelp = []string{
 	cmdGame + " <search>",
-	cmdGame + " -recent",
-	cmdGame + " -upcoming",
+	cmdGame + " -recent <pc/ps/mobile/nin/xbox>",
+	cmdGame + " -upcoming <pc/ps/mobile/nin/xbox>",
 }
 
 // From the /platforms endpoint.
@@ -47,6 +50,7 @@ var gamePlatforms = map[int]string{
 	24:  "Game Boy Advance",
 	32:  "Sega Saturn",
 	33:  "Game Boy",
+	34:  "Android",
 	35:  "Sega Game Gear",
 	36:  "Xbox Live Arcade",
 	37:  "Nintendo 3DS",
@@ -89,8 +93,17 @@ var gamePlatforms = map[int]string{
 	165: "PlayStation VR",
 }
 
-// GameResult represents game data returned from the API.
-type GameResult struct {
+// Pulled from relevant gamePlatforms; used in recent and upcoming subcommands.
+var platformCategories = map[string][]int{
+	"pc":     []int{3, 6, 13, 14, 82, 92},            // PC (Steam, etc.)
+	"ps":     []int{9, 38, 45, 46, 48, 165},          // PlayStation
+	"mobile": []int{34, 39, 55, 74},                  // Mobile games
+	"nin":    []int{5, 20, 37, 41, 47, 56, 130, 160}, // Nintendo
+	"xbox":   []int{11, 12, 36, 49},                  // Xbox
+}
+
+// Game represents game data returned from the API.
+type Game struct {
 	Name    string `json:"name"`
 	Summary string `json:"summary"`
 
@@ -98,12 +111,26 @@ type GameResult struct {
 	Platforms        []int `json:"platforms"`
 }
 
-func (g GameResult) ReleaseDate() string {
-	t := time.Unix(g.FirstReleaseDate, 0)
-	return fmt.Sprintf("%02d/%02d/%d", t.Month(), t.Day(), t.Year())
+// GameRelease represents game release data.
+type GameRelease struct {
+	ID   int64 `json:"id"`
+	Date int64 `json:"date"`
+	Game Game  `json:"game"`
 }
 
-func (g GameResult) PlatformString() string {
+func (g Game) Info() string {
+	return fmt.Sprintf("[%s] %s - %s", g.ReleaseDate(), g.PlatformString(), g.Name)
+}
+
+func (g Game) ReleaseDate() string {
+	return formatDate(g.FirstReleaseDate)
+}
+
+func (r GameRelease) ReleaseDate() string {
+	return formatDate(r.Date)
+}
+
+func (g Game) PlatformString() string {
 	out := []string{}
 	for _, p := range g.Platforms {
 		out = append(out, gamePlatforms[p])
@@ -147,7 +174,7 @@ func (cmd *GameCommand) Run(args ...string) {
 
 	switch cmdArgs[0] {
 	case "-recent":
-		cmd.recent(channel)
+		cmd.recent(channel, strings.Join(cmdArgs[1:], ""))
 	case "-upcoming":
 		cmd.upcoming(channel)
 	default:
@@ -172,12 +199,11 @@ func (cmd *GameCommand) search(channel, query string) {
 	// Released in the last 20 years.
 	releaseDate := strconv.FormatInt(time.Now().AddDate(-20, 0, 0).Unix(), 10)
 
-	cmd.bot.Log.WithField("query", query).Debug("GameCommand.search()")
 	req, err := api.NewRequest(
 		"POST",
 		igdbGamesURL,
 		api.Limit(1),
-		api.Fields("*"),
+		api.Fields("name, summary, first_release_date, platforms"),
 		api.Where("rating >= 80 & release_dates.date > "+releaseDate),
 		api.Search("", query),
 	)
@@ -186,25 +212,15 @@ func (cmd *GameCommand) search(channel, query string) {
 		return
 	}
 
-	req.Header.Add("user-key", cmd.bot.Config.IGDB.Key)
-	req.Header.Add("Accept", "application/json")
+	cmd.addHeaders(req)
 
-	resp, err := http.DefaultClient.Do(req)
+	content, err := getContentBytes(req)
 	if err != nil {
 		cmd.bot.Log.WithField("err", err).Error("GameCommand.search()")
 		return
 	}
 
-	content, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		cmd.bot.Log.WithField("err", err).Error("GameCommand.search()")
-		return
-	}
-
-	cmd.bot.Log.WithField("content", string(content)).Debug("GameCommand.search()")
-
-	results := make([]GameResult, 1)
+	results := make([]Game, 1)
 	err = json.Unmarshal(content, &results)
 	if err != nil {
 		cmd.bot.Log.WithField("err", err).Error("GameCommand.search()")
@@ -217,17 +233,79 @@ func (cmd *GameCommand) search(channel, query string) {
 	}
 	game := results[0]
 
-	cmd.bot.Log.WithField("game", game).Debug("GameCommand.search()")
-
-	info := fmt.Sprintf("[%s] %s - %s", game.ReleaseDate(), game.PlatformString(), game.Name)
-	cmd.bot.Msg(cmd.conn, channel, info)
+	cmd.bot.Msg(cmd.conn, channel, game.Info())
 	cmd.bot.Msg(cmd.conn, channel, game.Summary)
 }
 
-func (cmd *GameCommand) recent(channel string) {
-	cmd.bot.Log.Debug("GameCommand.recent()")
+func (cmd *GameCommand) recent(channel, platform string) {
+	if len(platformCategories[platform]) == 0 {
+		cmd.bot.Msg(cmd.conn, channel, "Unknown platform: "+platform)
+		cmd.Help()
+		return
+	}
+
+	// Games being released in the next 7 days.
+	date := strconv.FormatInt(time.Now().AddDate(0, 0, igdbTimeframe).Unix(), 10)
+
+	req, err := api.NewRequest(
+		"POST",
+		igdbReleaseDatesURL,
+		api.Limit(igdbLimit),
+		api.Fields("*, game.name, game.summary, game.first_release_date, game.platforms"),
+		api.Where(
+			fmt.Sprintf("platform = %s", joinPlatforms(platformCategories[platform])),
+			fmt.Sprintf("date > %s", date),
+		),
+		api.Sort("date", "asc"),
+	)
+	if err != nil {
+		cmd.bot.Log.WithField("err", err).Error("GameCommand.recent()")
+		return
+	}
+
+	cmd.addHeaders(req)
+
+	content, err := getContentBytes(req)
+	if err != nil {
+		cmd.bot.Log.WithField("err", err).Error("GameCommand.recent()")
+		return
+	}
+
+	results := make([]GameRelease, igdbLimit)
+	err = json.Unmarshal(content, &results)
+	if err != nil {
+		cmd.bot.Log.WithField("err", err).Error("GameCommand.recent()")
+		return
+	}
+
+	if len(results) <= 0 {
+		cmd.bot.Msg(cmd.conn, channel, "Nothing coming out in the next %d days.", igdbTimeframe)
+		return
+	}
+
+	for _, release := range results {
+		cmd.bot.Msg(cmd.conn, channel, "[%s] - %s", release.ReleaseDate(), release.Game.Name)
+	}
 }
 
 func (cmd *GameCommand) upcoming(channel string) {
 	cmd.bot.Log.Debug("GameCommand.upcoming()")
+}
+
+func (cmd *GameCommand) addHeaders(req *http.Request) {
+	req.Header.Add("user-key", cmd.bot.Config.IGDB.Key)
+	req.Header.Add("Accept", "application/json")
+}
+
+func formatDate(date int64) string {
+	t := time.Unix(date, 0)
+	return fmt.Sprintf("%02d/%02d/%d", t.Month(), t.Day(), t.Year())
+}
+
+func joinPlatforms(platformIDs []int) string {
+	s := fmt.Sprint(platformIDs)
+	s = strings.Replace(s, "[", "(", -1)
+	s = strings.Replace(s, "]", ")", -1)
+	s = strings.Join(strings.Fields(s), ",")
+	return s
 }
